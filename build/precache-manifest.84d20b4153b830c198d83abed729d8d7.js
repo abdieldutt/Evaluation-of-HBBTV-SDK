03b1d19cc36ce195fd2b5205055ab0b29c8ff4d6
Abdiel Dutt <abdiel@chargery.de>
2019-01-30 00:04:01 +0100

    initial commit

---
+++ build/precache-manifest.84d20b4153b830c198d83abed729d8d7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e92fb6d7f222b3b6ad838ae600aa860",
+    "url": "/static/media/newZealandVsEngland.7e92fb6d.jpg"
+  },
+  {
+    "revision": "bd5a362d4e4159656063",
+    "url": "/static/css/main.b307e61c.chunk.css"
+  },
+  {
+    "revision": "8296abc88c14c5ea086d",
+    "url": "/static/js/1.8296abc8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "edbc8e7816bb48553d0c16e0be4ce7dd",
+    "url": "/static/media/CricketWorldLogo.edbc8e78.jpg"
+  },
+  {
+    "revision": "3c8415b4a25460ec3d5c066db8c0a1db",
+    "url": "/static/media/austraVsSouthAfrica.3c8415b4.jpeg"
+  },
+  {
+    "revision": "bd5a362d4e4159656063",
+    "url": "/static/js/main.bd5a362d.chunk.js"
+  },
+  {
+    "revision": "e3f042cdf06622838c191c7c0554b295",
+    "url": "/static/media/australiaWon.e3f042cd.jpg"
+  },
+  {
+    "revision": "808b371fb2980ab34ccbf2c59f7b1cc6",
+    "url": "/static/media/englandWon.808b371f.jpg"
+  },
+  {
+    "revision": "2e5d8f87f23241d214c51e9cb81cc4b3",
+    "url": "/static/media/fieldstats.2e5d8f87.jpg"
+  },
+  {
+    "revision": "2e268a8083b51ccd22a429d442afc49e",
+    "url": "/static/media/statsenausnwl.2e268a80.png"
+  },
+  {
+    "revision": "bd34af6bf1971dd6a875ff70796babe3",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from './components/Home';
+import TodaysMatches from './components/TodaysMatches';
+import Summary from './components/Summary';
+import Statistics from './components/Statistics';
+import Rankings from './components/Rankings';
+import Error from './components/Error';
+import Navigation from './components/Navigation';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Navigation />
+            <Switch>
+              <Route path= '/' component= {Home} exact />
+              <Route path= '/todaysmatches' component= {TodaysMatches}  />
+              <Route path= '/summary' component= {Summary}  />
+              <Route path= '/statistics' component={Statistics}/>
+              <Route path= '/rankings' component={Rankings}/>
+              <Route component={Error} exact/>
+            </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
